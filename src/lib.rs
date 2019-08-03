6861eb8e1b066b171f569c861ef3807f149aad90
David Ackerman <enfipy@gmail.com>
2019-08-03 21:44:33 +0300

    Add json enum derive

--- src/lib.rs
+++ src/lib.rs
@@ -42,10 +42,56 @@ fn impl_serde_enum_derive(ast: &syn::DeriveInput) -> quote::Tokens {
     }
 }
 
+fn impl_serde_enum_struct_derive(ast: &syn::DeriveInput) -> quote::Tokens {
+    let name = &ast.ident;
+    quote! {
+        impl postgres::types::FromSql for #name {
+            fn from_sql(
+                _: &postgres::types::Type,
+                raw: &[u8],
+            ) -> Result<Self, Box<dyn std::error::Error + Sync + Send>> {
+                serde_json::from_slice(&raw).map_err(|_| Box::from("FATAL: Failed to deserialize data"))
+            }
+
+            fn accepts(ty: &postgres::types::Type) -> bool {
+                <serde_json::Value as postgres::types::FromSql>::accepts(ty)
+            }
+        }
+
+        impl postgres::types::ToSql for #name {
+            fn to_sql(
+                &self,
+                _: &postgres::types::Type,
+                out: &mut Vec<u8>,
+            ) -> Result<postgres::types::IsNull, Box<dyn std::error::Error + Sync + Send>>
+            where
+                Self: Sized,
+            {
+                *out = serde_json::to_vec(self)?;
+                Ok(postgres::types::IsNull::No)
+            }
+
+            fn accepts(ty: &postgres::types::Type) -> bool {
+                serde_json::Value::accepts(ty)
+            }
+
+            to_sql_checked!();
+        }
+    }
+}
+
 #[proc_macro_derive(EnumSql)]
 pub fn serde_enum_derive(input: TokenStream) -> TokenStream {
     let s = input.to_string();
     let ast = syn::parse_derive_input(&s).unwrap();
     let gen = impl_serde_enum_derive(&ast);
+    gen.parse().unwrap()
+}
+
+#[proc_macro_derive(JsonEnumSql)]
+pub fn serde_json_enum_derive(input: TokenStream) -> TokenStream {
+    let s = input.to_string();
+    let ast = syn::parse_derive_input(&s).unwrap();
+    let gen = impl_serde_enum_struct_derive(&ast);
     gen.parse().unwrap()
 }
